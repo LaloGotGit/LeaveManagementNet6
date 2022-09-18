fbe0e63b7f6811a147ea2b4cf5205830143757ef
LaloGotGit <eduardo.julian.san@gmail.com>
2022-09-18 02:07:43 -0500

    Added some modifications to the crud ui

---
+++ LeaveManagementWeb/Configuration/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagementWeb.Data;
+using LeaveManagementWeb.Models;
+
+namespace LeaveManagementWeb.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig() 
+        {
+            CreateMap<LeaveType,LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
